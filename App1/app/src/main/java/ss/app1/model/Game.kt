26259372033d77031bcf6fe8e9d4b987c9e7cd62
David Ford <dford@smart-soft.com>
2020-05-12 06:31:48 -0700

    Game msg, isGameOver

--- App1/app/src/main/java/ss/app1/model/Game.kt
+++ App1/app/src/main/java/ss/app1/model/Game.kt
@@ -7,6 +7,8 @@ class Game(shuffle: Boolean = true) {
     private var phi: Hand = Hand()
     private var dhi: Hand = Hand()
 
+    private var isStayInternal:Boolean = false
+
     init {
         deal()
     }
@@ -19,15 +21,42 @@ class Game(shuffle: Boolean = true) {
         while (dhi.points <= 17) {
             dhi = dhi.add(deck.takeCard())
         }
+        isStayInternal = true
     }
 
     fun deal() {
         phi = Hand().add(deck.takeCards(2))
         dhi = Hand().add(deck.takeCards(2))
+        isStayInternal = false
     }
 
     val ph:Hand get() = phi
     val dh:Hand get() = dhi
 
+    val isGameOver: Boolean get() = isStayInternal || ph.points >= 21
 
+    val msg: String
+        get() = if (isGameOver) {
+            when {
+                ph.points > 21 -> dealerWins
+                dh.points > 21 -> playerWins
+                ph.points == dh.points -> dealerWins
+                ph.points > dh.points -> playerWins
+                dh.points > ph.points -> dealerWins
+                else -> throw IllegalStateException()
+            }
+        } else {
+            pressHitOrStay
+        }
+
+    val isStay:Boolean get() = isStayInternal
+
+
+    companion object {
+        const val pressHitOrStay = "Press Hit or Stay"
+        const val dealerWins = "Dealer Wins!"
+        const val playerWins = "Player Wins!"
+
+
+    }
 }
--- App1/app/src/test/java/ss/app1/model/GameTest.kt
+++ App1/app/src/test/java/ss/app1/model/GameTest.kt
@@ -1,6 +1,6 @@
 package ss.app1.model
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 
@@ -35,12 +35,24 @@ class GameTest {
         assertEquals(3,g1.ph.points)  //a,2
         assertEquals(7,g1.dh.points)  //3,4
 
+        assertFalse(g1.isStay)
+        assertFalse(g1.isGameOver)
+        assertEquals(Game.pressHitOrStay, g1.msg)
+
         g1.hit()
         assertEquals(3,g1.ph.size)  //a,2
         assertEquals(2,g1.dh.size)  //3,4
 
+        assertFalse(g1.isStay)
+        assertFalse(g1.isGameOver)
+        assertEquals(Game.pressHitOrStay, g1.msg)
+
         assertEquals(8,g1.ph.points)  //a,2,5
         assertEquals(7,g1.dh.points)  //3,4
+
+        assertFalse(g1.isStay)
+        assertFalse(g1.isGameOver)
+        assertEquals(Game.pressHitOrStay, g1.msg)
 
         g1.stay()
         assertEquals(3,g1.ph.size)  //a,2
@@ -49,11 +61,20 @@ class GameTest {
         assertEquals(8,g1.ph.points)  //a,2,5
         assertEquals(20,g1.dh.points)  //3,4, 6, 7
 
+        assertTrue(g1.isStay)
+        assertTrue(g1.isGameOver)
+        assertEquals(Game.dealerWins, g1.msg)
+
         g1.deal()
         assertEquals(2,g1.ph.size)  //8,9
         assertEquals(2,g1.dh.size)  //10,J
 
         assertEquals(17,g1.ph.points)  //a,2,5
         assertEquals(20,g1.dh.points)  //3,4, 6,7
+
+        assertFalse(g1.isStay)
+        assertFalse(g1.isGameOver)
+        assertEquals(Game.pressHitOrStay, g1.msg)
+
     }
 }
